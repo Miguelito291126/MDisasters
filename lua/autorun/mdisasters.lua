0b7912c746a651a4bc4f5c915aa90276b7cc4626
Miguel Jimenez <miguelitojimenez291126@gmail.com>
2025-03-31 12:59:22 +0200

    change version

--- lua/autorun/mdisasters.lua
+++ lua/autorun/mdisasters.lua
@@ -1,7 +1,7 @@
 MDisasters = {}
 MDisasters.name = "MDisasters"
 MDisasters.author = "Miguelillo948"
-MDisasters.version = "0.0.2.2"
+MDisasters.version = "0.0.2.3"
 
 function MDisasters:msg(...)
     local args = {...}
